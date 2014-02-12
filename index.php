6fe70491d4ef3772f13ffe21345b6ec252248fe6
Thomas J Bradley <hey@thomasjbradley.ca>
2014-02-12 15:50:43 -0500

    Fixed some bugs.

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
 
+function goToMainSite () {
+  header('Location: http://17thousandpages.ca', true, 301);
+  exit;
+}
+
+if (empty($_SERVER['REDIRECT_URL'])) {
+  goToMainSite();
+}
+
 require_once 'urls.php';
 
 $shortUrl = str_replace('/', '', $_SERVER['REDIRECT_URL']);
 
 if (!isset($urls[$shortUrl])) {
-  header('Location: http://17thousandpages.ca', true, 301);
-  exit;
+  goToMainSite();
 }
 
 header('Location: ' . $urls[$shortUrl], true, 301);
